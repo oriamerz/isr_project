bd625a6c442f34f845346e8cd1cee762d1d6a739
oriamerz <oriamerz@gmail.com>
2021-07-08 22:51:33 +0300

    fix css

--- main.js
+++ main.js
@@ -6,8 +6,7 @@ var cities = [
         description: "בירת ישראל",
         Image: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT6zzbnB4lOKsmcW1qy18iQSrGwvZvgFTZtGQ&usqp=CAU',
         map: 'images/jerusalem_map.GIF',
-        icons: { position: [[33, 90], [10, 77], [76, 37],[76, 21]], text: ["blblbb", "mamamamam", "dldldldld", "dldldldld"] }
-
+        icons: { position: [[33, 90], [10, 77], [76, 37], [76, 21]], text: ["בית כנסת", "פארק", "מכולת", "ספריה"] }
     },
     {
         id: 2,
@@ -15,8 +14,7 @@ var cities = [
         description: "ישוב קהילתי דתי",
         Image: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/95/Kokhav_HaShahar_2020.jpg/540px-Kokhav_HaShahar_2020.jpg',
         map: 'images/kh_map.GIF',
-        icons: { position: [[20, 43], [76, 21], [76, 37],[44, 15],], text: ["dldldldld","blblbb", "mamamamam","gagagagag"] }
-
+        icons: { position: [[20, 43], [76, 21], [76, 37], [44, 15],], text: ["בית כנסת", "פארק", "מכולת", "ספריה"] }
     },
     {
         id: 3,
@@ -24,8 +22,7 @@ var cities = [
         description: "ישוב קהילתי ",
         Image: 'https://goyosh-storage.s3.eu-west-3.amazonaws.com/yeshuvim/rimonim.jpg',
         map: 'images/‏‏rimonim_map.GIF',
-        icons: { position: [[11, 55], [76, 37], [76, 3], [11, 2]], text: ["dldldldld","blblbb", "mamamamam","gagagagag"] }
-
+        icons: { position: [[11, 55], [76, 37], [76, 3], [11, 2]], text: ["בית כנסת", "פארק", "מכולת", "ספריה"] }
     },
     {
         id: 4,
@@ -33,8 +30,7 @@ var cities = [
         description: "העיר האדומה",
         Image: 'https://haifahaifa.co.il/wp-content/uploads/2019/06/55.jpg',
         map: '‏‏images/hifa_map.png',
-        icons: { position: [[89, 98], [44, 15], [76, 37],[20, 43]], text: ["blblbb","dldldldld", "mamamamam","gagagagag"] }
-
+        icons: { position: [[89, 98], [44, 15], [76, 37], [20, 43]], text: ["בית כנסת", "פארק", "מכולת", "ספריה"] }
     }
 ]
 
@@ -49,10 +45,11 @@ $(document).ready(function () {
             };
 
             $(".main_map").html(
-                '<div class="image_map" id ="image_map" ><img src=' + city.map + '>' +
+                '<img src=' + city.map + '>' +
                 icons_html +
-                '</img></div>'
+                '</img>'
             )
+
             $('.icon').mouseenter(function () {
                 $(this).append('<div class="popup" >' + city.icons.text[this.id] + '</div>')
                 $(".popup").css({ top: city.icons.position[this.id][0] });
@@ -67,7 +64,6 @@ $(document).ready(function () {
 })
 
 function fetchIssues() {
-
 
     var issuesList = document.getElementById('issuesList');
     issuesList.innerHTML = '';
@@ -86,8 +82,4 @@ function fetchIssues() {
             '</button>'
         '</div>';
     }
-
-
 }
-
-
